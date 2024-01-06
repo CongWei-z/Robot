af77013f419079d0d15d065167ae23b27c92d6ca
zhangyinghao <yinghaozhang@lilith.com>
2024-01-06 17:08:24 +0800

    添加了ssss

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def print_hi(name):
 
 # 按间距中的绿色按钮以运行脚本。
 if __name__ == '__main__':
-    print_hi('hello,世界1111111111111')
+    print_hi('hello,世界111111111qqqqq1111')
+    print("ssssss")
 
 # 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
